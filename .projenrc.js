a79d8d9a6851c772f9eafdc50f34e16b295d936c
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2023-02-27 08:43:00 -0800

    feat: repro https://github.com/aws/aws-cdk/issues/24314

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,8 @@
-const { awscdk } = require('projen');
+const { awscdk, javascript } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'repro-ecr-policy-with-resource',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  packageManager: javascript.PackageManager.NPM,
 });
 project.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
+import { AnyPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
-export class MyStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps = {}) {
+export class EcrStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // define resources here...
+    const r = new Repository(this, 'MyEcrRepo', {
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    r.addToResourcePolicy(new PolicyStatement({
+      actions: ['ecr:GetDownloadUrlForLayer'],
+      principals: [new AnyPrincipal()],
+      // Every other resource policy I've ever written requires a resource.
+      // They're really only good for limiting access within a resource.
+      // I understand why they're allowed, but not why they're required.
+      // Strangely, not only does Cfn for ECR not allow
+      // us to specify a resource policy for a specific image,
+      // it will straight up fail if a resource section is present at all.
+      resources: ['*'], // This will break, so CDK should warn. At least until Cfn (and possibly ECR) is fixed.
+    }));
   }
 }
 
@@ -16,8 +31,6 @@ const devEnv = {
 };
 
 const app = new App();
-
-new MyStack(app, 'repro-ecr-policy-with-resource-dev', { env: devEnv });
-// new MyStack(app, 'repro-ecr-policy-with-resource-prod', { env: prodEnv });
+new EcrStack(app, 'MyEcrStack',  { env: devEnv });
 
 app.synth();
